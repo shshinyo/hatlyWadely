ec3100449f47c3eae43238443dd89844b990eccc
M-Hamo <58711682+M-Hamo@users.noreply.github.com>
2021-08-12 11:39:00 +0200

    using imbedded js function

--- wasaleFront/src/app/shared/pipes/item-name.pipe.ts
+++ wasaleFront/src/app/shared/pipes/item-name.pipe.ts
@@ -4,12 +4,8 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "itemName",
 })
 export class ItemName implements PipeTransform {
-  transform(input: string): string {
-    const words = input.split(" ");
-    // just change size value to change your word
-    const size = 4;
-    const pipedWord = words.splice(0, size).join(" ");
-
-    return pipedWord + "...";
+  transform(input: string, size: number): string {
+    let substring = input.substr(0, size);
+    return substring + "...";
   }
 }
