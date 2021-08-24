30ae226ee7fd428304cd4aea8291115c1a248c41
M-Hamo <58711682+M-Hamo@users.noreply.github.com>
2021-08-24 13:11:00 +0200

    ...

--- wasaleFront/src/app/core/services/auth.service.ts
+++ wasaleFront/src/app/core/services/auth.service.ts
@@ -1,74 +1,52 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
-import { Subject } from "rxjs";
-import { newUser } from "src/app/shared/utilities/authUser";
+import { Observable, Subject, throwError } from "rxjs";
+import { catchError } from "rxjs/Operators";
+
 import { environment } from "src/environments/environment";
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  currentUser: newUser;
+  private _url = `${environment.api_url}api/users`;
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" }),
+  };
+
+  currentUser;
   redirectUrl;
 
   errorMessageSubject = new Subject<string>();
-  errorMessage$ = this.errorMessageSubject.asObservable();
 
   get isLoggedIn(): boolean {
     return !!window.localStorage.getItem("user");
-    // return !!this.currentUser;
   }
-  constructor(private router: Router,private http:HttpClient, private toastr: ToastrService) {}
+  constructor(private _http: HttpClient) {}
 
-  login(body) {
-    return this.http.post(`${environment.api_url_ip}api/users/login`,body,{
-      observe:'body',
-      withCredentials:true,
-      headers:new HttpHeaders().append('Content-Type','application/json')
-
-    })
+  login(user) {
+    return this._http
+      .post(`${this._url}/login`, user, this.httpOptions)
+      .pipe(catchError(this._handleError));
   }
-  register(body) {
-    return this.http.post(`${environment.api_url_ip}api/users/newUser`,body,{
-      observe:'body',
-      withCredentials:true,
-      headers:new HttpHeaders().append('Content-Type','application/json')
-
-    })
+  register(newUser) {
+    return this._http
+      .post(`${this._url}/newUser`, newUser, this.httpOptions)
+      .pipe(catchError(this._handleError));
   }
+
   logOut(): void {
     this.currentUser = null;
     window.localStorage.removeItem("user");
   }
-}
 
-export const localUsers: newUser[] = [
-  {
-    id: 1,
-    name: "mohamed eldeeb",
-    phone: "01098799837",
-    location: "كوم حمادة",
-    email: "mohamed.eldeib5@gmail.com",
-    password: "hamo222",
-    role: 1,
-  },
-  {
-    id: 2,
-    name: "mohamed ",
-    phone: "01098799837",
-    location: "كوم حمادة",
-    email: "mohamed.eldeeb@gmail.com",
-    password: "hamo012",
-    role: 0,
-  },
-  {
-    id: 3,
-    name: "soso mostafa karem",
-    phone: "01098799837",
-    location: "كوم حمادة",
-    email: "moahmned.eldeiib@gmail.com",
-    password: "hamo012548",
-    role: 0,
-  },
-];
+  private _handleError(err: any): Observable<never> {
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
+  }
+}
